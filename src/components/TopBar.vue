2a133e73b4418fa0f6032c19e90950e3c5a25ea5
ecdns <clementpavot78@gmail.com>
2023-03-07 08:58:36 +0100

    clear code

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,18 +1,11 @@
 <template>
     <q-header elevated>
         <q-toolbar>
-            <q-btn
-            flat
-            dense
-            round
-            icon="menu"
-            aria-label="Menu"
-            @click="toggleLeftDrawer"
-            />
+            <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
             <q-toolbar-title>
                 CRM du Cube 3
             </q-toolbar-title>
-            
+
             <div>
                 <q-btn color="purple" label="Account Settings">
                     <q-menu>
@@ -22,23 +15,17 @@
                                 <q-toggle v-model="mobileData" label="Use Mobile Data" />
                                 <q-toggle v-model="bluetooth" label="Bluetooth" />
                             </div>
-                            
+
                             <q-separator vertical inset class="q-mx-lg" />
-                            
+
                             <div class="column items-center">
                                 <q-avatar size="72px">
                                     <img src="https://cdn.quasar.dev/img/avatar4.jpg">
                                 </q-avatar>
-                                
+
                                 <div class="text-subtitle1 q-mt-md q-mb-xs">John Doe</div>
-                                
-                                <q-btn
-                                color="primary"
-                                label="Logout"
-                                push
-                                size="sm"
-                                v-close-popup
-                                />
+
+                                <q-btn color="primary" label="Logout" push size="sm" v-close-popup />
                             </div>
                         </div>
                     </q-menu>
@@ -46,37 +33,23 @@
             </div>
         </q-toolbar>
     </q-header>
-    <q-drawer
-    v-model="leftDrawerOpen"
-    show-if-above
-    bordered
-    >
-    <q-item-label
-    header
-    >
-    Liens
-</q-item-label>
-    <q-list>
-        <q-item
-        clickable
-        v-for="link in essentialLinks"
-        :key="link.title"
-        :to=link.link
-        >
-            <q-item-section
-              v-if="link.icon"
-              avatar
-            >
-              <q-icon :name="link.icon" />
-            </q-item-section>
-        
-            <q-item-section>
-              <q-item-label>{{ link.title }}</q-item-label>
-              <q-item-label caption>{{ link.caption }}</q-item-label>
-            </q-item-section>
-          </q-item>
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+        <q-item-label header>
+            Liens
+        </q-item-label>
+        <q-list>
+            <q-item clickable v-for="link in essentialLinks" :key="link.title" :to=link.link>
+                <q-item-section v-if="link.icon" avatar>
+                    <q-icon :name="link.icon" />
+                </q-item-section>
+
+                <q-item-section>
+                    <q-item-label>{{ link.title }}</q-item-label>
+                    <q-item-label caption>{{ link.caption }}</q-item-label>
+                </q-item-section>
+            </q-item>
         </q-list>
-        </q-drawer>
+    </q-drawer>
 </template>
 
 <script>
@@ -84,24 +57,24 @@
 import { ref } from 'vue';
 
 const linksList = [
-{
-title: 'Accueil',
-caption: 'quasar.dev',
-icon: 'school',
-link: '/'
-},
-{
-title: 'Nouveau lien',
-caption: 'Lien du CRM',
-icon: 'favorite',
-link: '/settings'
-},
-{
-title: 'Mes messages',
-caption: 'Messages',
-icon: 'favorite',
-link: '/messages'
-},
+    {
+        title: 'Accueil',
+        caption: 'quasar.dev',
+        icon: 'school',
+        link: '/'
+    },
+    {
+        title: 'Nouveau lien',
+        caption: 'Lien du CRM',
+        icon: 'favorite',
+        link: '/settings'
+    },
+    {
+        title: 'Mes messages',
+        caption: 'Messages',
+        icon: 'favorite',
+        link: '/messages'
+    },
 ]
 
 export default {
