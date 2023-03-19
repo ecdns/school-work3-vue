c0f5b86b41d295b7277e8e67616af38d9a49b9c7
ecdns <clementpavot78@gmail.com>
2023-03-19 13:03:37 +0100

    ProjectListe + ProjectCard

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,22 @@
+<template>
+    <q-page>
+        <q-list>
+            <ProjectCard v-for="(project, id) in projects" :project="project" v-bind:key="id" />
+        </q-list>
+    </q-page>
+</template>
+
+<script>
+
+import ProjectCard from './ProjectCard.vue';
+
+export default {
+    components: {
+        ProjectCard
+    },
+    props: {
+        projects: Array
+    }
+}
+
+</script>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,6 +1,79 @@
 <template>
     <q-page class="flex flex-center">
-        <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px">
+        <ProjectList :projects="items" />
     </q-page>
 </template>
-  
+
+<script>
+
+import ProjectList from 'src/components/ProjectList.vue';
+
+export default {
+    components: {
+        ProjectList
+    },
+    data() {
+        return {
+            items: [
+                {
+                    id: 1,
+                    name: "Projet 1",
+                    createdAt: "2023-03-20",
+                    members: [
+                        {
+                            id: 1,
+                            name: "Clément"
+                        },
+                        {
+                            id: 6,
+                            name: "Clément"
+                        },
+                        {
+                            id: 7,
+                            name: "Clément"
+                        },
+                        {
+                            id: 8,
+                            name: "Clément"
+                        },
+                    ],
+                    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic sed impedit ipsam in? Maiores quod quae, vitae voluptates similique facere tempora ipsam, fugiat magni voluptas, animi beatae voluptatibus quas. Mollitia?",
+                    statut: {
+                        name: "In Progress",
+                        color: "teal"
+                    }
+                },
+                {
+                    id: 2,
+                    name: "Projet 2",
+                    createdAt: "2022-12-10",
+                    members: [
+                        {
+                            id: 2,
+                            name: "Macky"
+                        },
+                        {
+                            id: 3,
+                            name: "Macky"
+                        },
+                        {
+                            id: 4,
+                            name: "Macky"
+                        },
+                        {
+                            id: 5,
+                            name: "Macky"
+                        },
+                    ],
+                    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic sed impedit ipsam in? Maiores quod quae, vitae voluptates similique facere tempora ipsam, fugiat magni voluptas, animi beatae voluptatibus quas. Mollitia?",
+                    statut: {
+                        name: "Stopped",
+                        color: "red"
+                    }
+                },
+            ]
+        }
+    }
+}
+
+</script>
