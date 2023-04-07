0e0851dbf00570af82c58dc2a93658309175c5c9
Macky Tall <tallmacky001@gmail.com>
2023-04-07 11:23:28 +0200

    add new customer form popup + local customer add

--- src/components/CustomerListing.vue
+++ src/components/CustomerListing.vue
@@ -1,12 +1,58 @@
 <template>
   <q-page>
-    <div class="icon_row">
-      <p class="add_icon">
-        <q-icon class="control_point_icon" name="control_point" size="30px" color="blue" />
-      </p>
+    <div class="q-pa-md">
+      <div class="add_customer_button">
+        <q-btn push outline rounded color="primary" icon="add" label="NOUVEAU CLIENT">
+          <q-popup-proxy>
+            <q-banner>
+              <template v-slot:avatar>
+                <q-form action="" method="post" @submit.prevent.stop="onSubmit">
+                  <div class="flex-input">
+                    <q-input outlined v-model="lastName" name="lastName" ref="firstNameRef" label="Nom"
+                      class="q-mx-lg q-my-sm" />
+                    <q-input outlined v-model="firstName" name="firstName" ref="firstNameRef" label="Prénom"
+                      class="q-mx-lg q-my-sm" />
+                  </div>
+                  <div class="flex-input">
+                    <q-input outlined v-model="customerCompanyName" name="customerCompanyName"
+                      ref="customerCompanyNameRef" label="Société" class="q-mx-lg q-my-sm" />
+                    <q-input outlined v-model="post" name="post" ref="postRef" label="Rôle" class="q-mx-lg q-my-sm" />
+                  </div>
+                  <div class="flex-input">
+                    <q-input outlined v-model="phone" name="phone" ref="phoneRef" label="Téléphone"
+                      class="q-mx-lg q-my-sm" />
+                    <q-input outlined v-model="email" name="email" ref="emailRef" label="Email" class="q-mx-lg q-my-sm" />
+                  </div>
+                  <div class="flex-input">
+                    <q-input outlined v-model="address" name="address" ref="addressRef" label="Adresse"
+                      class="q-mx-lg q-my-sm" />
+                    <q-input outlined v-model="country" name="country" ref="countryRef" label="Pays"
+                      class="q-mx-lg q-my-sm" />
+                  </div>
+                  <div class="flex-input">
+                    <q-input outlined v-model="zipCode" name="zipCode" ref="zipCodeRef" label="Code Postal"
+                      class="q-mx-lg q-my-sm" />
+                    <q-input outlined v-model="city" name="city" ref="cityRef" label="Ville" class="q-mx-lg q-my-sm" />
+                  </div>
 
+
+                  <div class="div_submit_button">
+                    <q-btn class="submit_button" outlined ripple label="Valider" type="submit" color="primary" />
+
+                  </div>
+                </q-form>
+
+              </template>
+            </q-banner>
+          </q-popup-proxy>
+        </q-btn>
+      </div>
 
     </div>
+
+
+
+
     <div class="q-pa-md">
       <q-table flat bordered title="Clients" :rows="rows" :columns="columns" row-key="name"
         :selected-rows-label="getSelectedString" selection="multiple" v-model:selected="selected" />
@@ -21,6 +67,7 @@
 
 <script>
 import { ref } from 'vue'
+import useQuasar from 'quasar/src/composables/use-quasar.js'
 
 const columns = [
   {
@@ -33,103 +80,136 @@ const columns = [
     sortable: true
   },
   { name: 'email', align: 'center', label: 'Email', field: 'email', sortable: true },
-  { name: 'société', label: 'Société', field: 'Société', sortable: true },
-  { name: 'rôle', label: 'Rôle', field: 'rôle' },
+  { name: 'customerCompanyName', label: 'Société', field: 'customerCompanyName', sortable: true },
+  { name: 'post', label: 'Rôle', field: 'post' },
 
 ]
 
-const rows = [
+let rows = ref([
   {
     name: 'Frozen Yogurt',
     email: 'freddyyogurt@gmail.com',
-    Société: 'Amazon',
-    rôle: 'Manager',
-    protein: 4.0,
-    sodium: 87,
-    calcium: '14%',
-    iron: '1%'
+    customerCompanyName: 'Amazon',
+    post: 'Manager',
+
   },
   {
     name: 'Froze Yogurt',
     email: 'freddyryogurt@gmail.com',
-    Société: 'Amazon',
-    rôle: 'Manager',
+    customerCompanyName: 'Amazon',
+    post: 'Manager',
 
   },
-  {
-    name: 'Frozen Ygurt',
-    email: 'freddyyogurt@gmail.com',
-    Société: 'Amazon',
-    rôle: 'Manager',
-    protein: 4.0,
-    sodium: 87,
-    calcium: '14%',
-    iron: '1%'
-  },
-  {
-    name: 'Frozen Yogut',
-    email: 'freddyyogurt@gmail.com',
-    Société: 'Amazon',
-    rôle: 'Manager',
-    protein: 4.0,
-    sodium: 87,
-    calcium: '14%',
-    iron: '1%'
-  },
-  {
-    name: 'Frozen Yogur',
-    email: 'freddyyogurt@gmail.com',
-    Société: 'Amazon',
-    rôle: 'Manager',
-    protein: 4.0,
-    sodium: 87,
-    calcium: '14%',
-    iron: '1%'
-  },
-  {
-    name: 'Fozen Yogurt',
-    email: 'freddyyogurt@gmail.com',
-    Société: 'Amazon',
-    rôle: 'Manager',
-    protein: 4.0,
-    sodium: 87,
-    calcium: '14%',
-    iron: '1%'
-  },
-  {
-    name: 'Frozen Yogu',
-    email: 'freddyyogurt@gmail.com',
-    Société: 'Amazon',
-    rôle: 'Manager',
-    protein: 4.0,
-    sodium: 87,
-    calcium: '14%',
-    iron: '1%'
-  },
-  {
-    name: 'Frozen Yogurt',
-    email: 'freddyyogurt@gmail.com',
-    Société: 'Amazon',
-    rôle: 'Manager',
-    protein: 4.0,
-    sodium: 87,
-    calcium: '14%',
-    iron: '1%'
-  },
 
-]
+
+])
 
 export default {
+
   setup() {
+    const $q = useQuasar()
     const selected = ref([])
+
+    const lastName = ref(null)
+    const lastNameRef = ref(null)
+
+    const firstName = ref(null)
+    const firstNameRef = ref(null)
+
+    const email = ref(null)
+    const emailRef = ref(null)
+
+    const address = ref(null)
+    const addressRef = ref(null)
+
+    const city = ref(null)
+    const cityRef = ref(null)
+
+    const zipCode = ref(null)
+    const zipCodeRef = ref(null)
+
+    const phone = ref(null)
+    const phoneRef = ref(null)
+
+    const customerCompanyName = ref(null)
+    const customerCompanyNameRef = ref(null)
+
+    const post = ref(null)
+    const postRef = ref(null)
+
 
     return {
       selected,
       columns,
       rows,
 
+      lastName,
+      lastNameRef,
+
+      firstName,
+      firstNameRef,
+      email,
+      emailRef,
+
+      address,
+      addressRef,
+
+      city,
+      cityRef,
+
+      zipCode,
+      zipCodeRef,
+
+      phone,
+      phoneRef,
+
+      customerCompanyName,
+      customerCompanyNameRef,
+
+      post,
+      postRef,
+
+
+
+      onSubmit() {
+        firstNameRef.value.validate()
+
+
+        if (firstNameRef.value.hasError) {
+          // form has error
+        }
+
+        else {
+          // console.log(rows)
+          rows.value.unshift(
+            {
+              name: lastName.value + ' ' + firstName.value,
+              email: email.value,
+              customerCompanyName: customerCompanyName.value,
+              post: post.value,
+              address: address.value,
+              city: city.value,
+              zipCode: zipCode.value,
+              phone: phone.value
+
+            }
+          )
+          console.log(rows.value)
+
+          // $q.notify({
+          //   icon: 'done',
+          //   color: 'positive',
+          //   message: 'Submitted'
+          // })
+
+        }
+      },
+
+
+
+
       getSelectedString() {
-        return selected.value.length === 0 ? '' : `${selected.value.length} record${selected.value.length > 1 ? 's' : ''} selected of ${rows.length}`
+        return selected.value.length === 0 ? '' : `${selected.value.length} record${selected.value.length > 1 ? 's' : ''} selected of ${rows.value.length}`
       }
     }
   }
@@ -137,19 +217,21 @@ export default {
 </script>
 
 <style lang="sass" >
-.icon_row
-  // border: 2px solid blue
-
-  .add_icon
-    // border: 2px solid blue
-    position: absolute
-    z-index: 1
-    right: 30px
-    top: 30px
-
-    .control_point_icon
+.add_customer_button
+  position: absolute
+  z-index: 1
+  right: 30px
+  top: 70px
+  cursor:  pointer
 
 
+.flex-input
+  display: flex
+
+.submit_button
+  width:100px
+  margin:20px auto
+  display: block
 
 
 </style>
